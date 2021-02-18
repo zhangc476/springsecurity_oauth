b0e9a66c8d4c4445f7ee7b0dd29fa4cdf9600e4c
zhangc476 <zhangc476@chinaunicom.cn>
2021-02-18 14:32:33 +0800

    改进工具类

---
+++ oauth2_server/src/main/java/com/utils/Payload.java
@@ -0,0 +1,12 @@
+package com.utils;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class Payload<T> {
+    private String id;
+    private T userInfo;
+    private Date expiration;
+}
